a8b86109b3e103904f14d3a42b73748eb5dd38aa
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-25 19:20:12 +0530

    handler for reading uart buffer at periodic intervals

--- src/main.c
+++ src/main.c
@@ -1,11 +1,41 @@
 #include "mgos.h"
 
-static void timer_cb(void *arg) {
+int uart_no = 1;
 
-  (void) arg;
+static void check_uart(void *arg)
+{
+    int bytes_avail = mgos_uart_read_avail(uart_no);
+    char buf[100];
+    if (bytes_avail > 0)
+    {
+        mgos_uart_read(uart_no, buf, bytes_avail);
+        LOG(LL_INFO, ("Received information: %s", buf));
+    }
+    else {
+        LOG(LL_INFO, ("No information available."));
+    }
+    (void)arg;
 }
 
-enum mgos_app_init_result mgos_app_init(void) {
-  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
-  return MGOS_APP_INIT_SUCCESS;
+enum mgos_app_init_result mgos_app_init(void)
+{
+    LOG(LL_INFO, ("Preparing UART interface"));
+
+    struct mgos_uart_config ucfg;
+    mgos_uart_config_set_defaults(uart_no, &ucfg);
+
+    ucfg.baud_rate = 9600;
+    ucfg.rx_buf_size = 150;
+    ucfg.tx_buf_size = 150;
+
+    if (!mgos_uart_configure(uart_no, &ucfg))
+    {
+        LOG(LL_ERROR, ("Failed to configure UART%d", uart_no));
+    }
+
+    // mgos_uart_set_dispatcher(uart_no, uart_msg_recv, NULL);
+    mgos_set_timer(1000, MGOS_TIMER_REPEAT, check_uart, NULL);
+    LOG(LL_INFO, ("Handler set for receiving data via UART."));
+
+    return MGOS_APP_INIT_SUCCESS;
 }
