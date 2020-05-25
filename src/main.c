f70b148c1e77e913197d66ad4d588c3e374951f6
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-25 17:16:00 +0530

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "mgos.h"
+
+static void timer_cb(void *arg) {
+
+  (void) arg;
+}
+
+enum mgos_app_init_result mgos_app_init(void) {
+  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+  return MGOS_APP_INIT_SUCCESS;
+}
